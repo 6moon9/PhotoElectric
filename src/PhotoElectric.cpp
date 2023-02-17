14bc9cb00dcd77b973088882622f03550aa718f7
SimonPucheu <104694344+SimonPucheu@users.noreply.github.com>
2023-02-17 10:02:16 +0100

    Add a delay

--- src/PhotoElectric.cpp
+++ src/PhotoElectric.cpp
@@ -2,23 +2,29 @@
 
 #include "PhotoElectric.h"
 
-PhotoElectric::PhotoElectric(uint8_t iPin)
+PhotoElectric::PhotoElectric(uint8_t iPin, int iMinCount = 1)
 {
     pin = iPin;
+    minCount = iMinCount;
     pinMode(pin, INPUT);
 }
 
 /**
  * Get the value of the photo electric's pin
- * 
+ *
  * @return bool true if an object obstructs the sensor, false otherwise
  */
 bool PhotoElectric::detects()
 {
     if (!state())
     {
-        while (!state());
-        return true;
+        // while (!state());
+        count++;
+        if (count >= minCount)
+        {
+            count = 0;
+            return true;
+        }
     }
     return false;
 }
--- src/PhotoElectric.h
+++ src/PhotoElectric.h
@@ -3,8 +3,10 @@
 class PhotoElectric
 {
     public:
-        PhotoElectric(uint8_t iPin);
+        PhotoElectric(uint8_t iPin, int iMinCount = 1);
         bool detects();
         bool state();
         uint8_t pin;
+        int minCount;
+        int count = 0;
 };
